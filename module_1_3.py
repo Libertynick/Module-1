cc39c874c14ce7186d408a0a9b7dbfbc096af0b3
Nick Fedorenko <nfm12121994@gmail.com>
2024-11-05 15:01:37 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Nick'
+print (name,type(name))
+age = 29
+print (age, type (age))
+New_Age = age + 100
+print (New_Age, type (New_Age))
+is_student = True
+print (is_student, type (is_student))

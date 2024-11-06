df108a9b41cb0abc305aa97cdd9a4f5a24182532
Nick Fedorenko <nfm12121994@gmail.com>
2024-11-06 17:10:10 +0300

    Add files via upload

---
+++ Peremennye.py
@@ -0,0 +1,5 @@
+quantity_complete_DZ = 12
+quantity_hours = 1.5
+name_of_course = 'Phyton'
+time_per1_task = quantity_hours / quantity_complete_DZ
+print(name_of_course, ', всего задач:', quantity_complete_DZ,', затрачено часов:',quantity_hours,', среднее время выполнения',time_per1_task,'часа')
